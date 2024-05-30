a5caa2bfbdffcc02e825784eeeeb6764766a6a7c
hrithik001 <itsmehrithikroshan@gmail.com>
2024-05-30 11:30:17 +0530

    fixed session timeout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :set_current_user
+    before_action :check_session_timeout
     
     def set_current_user
         if session[:user_session_id]
@@ -16,5 +17,17 @@ class ApplicationController < ActionController::Base
     def user_logged_in?
         redirect_to sign_in_path, alert: 'Please sign in first' if Current.user.nil?
     end
+    
+    def check_session_timeout
+        if session[:user_session_id]
+          latest_session = ActiveSession.find_by(session_id: session[:user_session_id])
+          puts latest_session.session_expiry, Time.current, "++++++++++++++++++++++++"
+          if latest_session && Time.current > latest_session.session_expiry
+            session[:user_session_id] = nil
+            reset_session
+            redirect_to sign_in_path, alert: "Your session has timed out. Please sign in again."
+          end
+        end
+      end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,10 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:email])
         if user.present? && user.authenticate(params[:password]) && user[:role] == params[:role]
             session_id = SecureRandom.uuid
-            @user_active_session = ActiveSession.new(user_id: user.id, session_id: session_id, session_expiry: 15.minutes.from_now, session_status: true)
+            @user_active_session = ActiveSession.new(user_id: user.id, session_id: session_id, session_expiry: 30.seconds.from_now, session_status: true)
             @user_active_session.save
             session[:user_session_id] = session_id
+            puts session[:user_session_id], "############################################"
             redirect_to root_path,notice: "logged in successfully as #{user[:role]}"
         else
             flash[:alert] = "Not valid "
